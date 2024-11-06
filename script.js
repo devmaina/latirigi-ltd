050f80fce2836c027f48ed86a7dee5a2313f2613
mainadev <62764842+devmaina@users.noreply.github.com>
2024-11-06 19:10:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const slideshow = document.querySelector('.slideshow');
-const imagesDir = 'images/';
+const imagesDir = '/';
 const imageFiles = [
     'image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 
     'image5.jpg'
